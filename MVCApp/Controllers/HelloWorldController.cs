050380c451d33dd097e0fecd9e103282945031c2
Luis <luis.angel.larh8586@gmail.com>
2024-03-07 20:22:56 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome (string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name ;
+            ViewData["NumTimes"] = numTimes ;
+            return View();
+        }
+
+        //Get: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        //Get: /HelloWorld/Welcome/
+        //Requires using System.Text.Encodings.Web
+        //Get: /HelloWorld/Welcome?name=luis&numtimes=5/
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is the welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+        //}
+    }
+}
---
+++ MVCApp/Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using System;
+using System.Linq;
+
+namespace MVCApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MVCAppContext>>()))
+            {
+                //Look for any movies
+                if (context.Movie.Any())
+                {
+                    return;//Db has been seeded
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Dune",
+                        ReleaseDate = DateTime.Parse("2018-2-12"),
+                        Genre = "Action",
+                        Price = 40.3M, 
+                        Rating = "M"
+                    },
+                    new Movie
+                    {
+                        Title = "El sorprendente Hombre Araña",
+                        ReleaseDate = DateTime.Parse("2008-5-18"),
+                        Genre = "SuperHeroes",
+                        Price = 90.40M,
+                        Rating = "M"
+                    },
+                    new Movie
+                    {
+                        Title = "Iron Man",
+                        ReleaseDate = DateTime.Parse("2012-6-20"),
+                        Genre = "SuperHeroes",
+                        Price = 120M,
+                        Rating = "M"
+                    },
+                    new Movie
+                    {
+                        Title = "Toy Story",
+                        ReleaseDate = DateTime.Parse("2000-10-20"),
+                        Genre = "Animation",
+                        Price = 80.20M,
+                        Rating = "M"
+                    },
+                    new Movie
+                    {
+                        Title = "Cars 3",
+                        ReleaseDate = DateTime.Parse("2017-10-2"),
+                        Genre = "Animation",
+                        Price = 120M,
+                        Rating = "M"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
